d933db026a1328e304115926801ec3152784798a
Kristina Todd <Kristina.Todd22@gmail.com>
2020-02-29 22:31:40 -0700

    styling updates

--- clientsrc/src/components/posts.vue
+++ clientsrc/src/components/posts.vue
@@ -1,27 +1,29 @@
 <template>
-  <div class="col-12 col-md-4 mt-3">
-    <router-link :to="{name: 'PostDetails', params: {postId: this.postData.id}}">
-      <div @click="setActive" class="card m-auto" style="width: 18rem;">
-        <img :src="postData.imgUrl" class="card-img-top" />
-        <div class="card-body">
-          <h5 class="card-title">{{postData.name}}</h5>
-        </div>
+  <router-link :to="{name: 'PostDetails', params: {postId: this.postData.id}}">
+    <div @click="setActive" class="card m-auto" style="width: 18rem;">
+      <img :src="postData.imgUrl" class="card-img-top" />
+      <div class="card-body text-center">
+        <h5 class="name text-info">{{postData.name}}</h5>
       </div>
-    </router-link>
-  </div>
+    </div>
+  </router-link>
 </template>
 
 <script>
-export default {
-  name: "Post",
-  props: ["postData", "postIndex"],
-  methods: {
-    setActive() {
-      this.$store.dispatch("setActivePost", this.postData);
+  export default {
+    name: "Post",
+    props: ["postData", "postIndex"],
+    methods: {
+      setActive() {
+        this.$store.dispatch("setActivePost", this.postData);
+      }
     }
-  }
-};
+  };
 </script>
 
 <style scoped>
+  .name {
+    font-family: "Pacifico";
+    font-size: xx-large;
+  }
 </style>
