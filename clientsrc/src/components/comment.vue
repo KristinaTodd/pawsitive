39702c5f76412bc2d4e35f5b238c45f4ef5fab3b
Noah-Preston <noah.preston2@gmail.com>
2020-03-02 09:39:14 -0700

    made postdetails mobile friendly

--- clientsrc/src/components/comment.vue
+++ clientsrc/src/components/comment.vue
@@ -7,38 +7,42 @@
           <div class="col-9 text-left justify-content-center name p-1">
             <h5>{{commentData.content}}</h5>
           </div>
-          <div class="col-3 text-right text-secondary">By: {{commentData.creatorEmail}}</div>
+          <div class="col-12 text-right text-secondary">By: {{commentData.creatorEmail}}</div>
           <!-- <div class="col-10"></div> -->
-          <div class="col-12 justify-content-end p-1"><button class="btn btn-danger"
-              v-if="commentData.creatorEmail == activeProfile.email" @click="deleteComment">Delete</button></div>
+          <div class="col-12 justify-content-end p-1">
+            <button
+              class="btn btn-danger"
+              v-if="commentData.creatorEmail == activeProfile.email"
+              @click="deleteComment"
+            >Delete</button>
+          </div>
         </div>
       </div>
       <div class="col-2"></div>
     </div>
   </div>
-
 </template>
 
 <script>
-  export default {
-    name: "Comment",
-    methods: {
-      deleteComment() {
-        this.$store.dispatch("deleteComment", this.commentData.id);
-      }
-    },
-    computed: {
-      activeProfile() {
-        return this.$store.state.profile;
-      }
-    },
-    props: ["commentData", "commentIndex"]
-  };
+export default {
+  name: "Comment",
+  methods: {
+    deleteComment() {
+      this.$store.dispatch("deleteComment", this.commentData.id);
+    }
+  },
+  computed: {
+    activeProfile() {
+      return this.$store.state.profile;
+    }
+  },
+  props: ["commentData", "commentIndex"]
+};
 </script>
 
 <style scoped>
-  .name {
-    font-family: "Pacifico";
-    font-size: xx-large;
-  }
+.name {
+  font-family: "Pacifico";
+  font-size: xx-large;
+}
 </style>
--- clientsrc/src/components/posts.vue
+++ clientsrc/src/components/posts.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link :to="{name: 'PostDetails', params: {postId: this.postData.id}}">
     <div @click="setActive" class="card m-auto" style="width: 18rem;">
-      <img :src="postData.imgUrl" class="card-img-top" />
+      <img :src="postData.imgUrl" class="card-img-top img-fluid" />
       <div class="card-body text-center">
         <h5 class="name text-info">{{postData.name}}</h5>
         <p class="text-secondary">{{postData.creatorEmail}}</p>
@@ -11,20 +11,20 @@
 </template>
 
 <script>
-  export default {
-    name: "Post",
-    props: ["postData", "postIndex"],
-    methods: {
-      setActive() {
-        this.$store.dispatch("setActivePost", this.postData);
-      }
+export default {
+  name: "Post",
+  props: ["postData", "postIndex"],
+  methods: {
+    setActive() {
+      this.$store.dispatch("setActivePost", this.postData);
     }
-  };
+  }
+};
 </script>
 
 <style scoped>
-  .name {
-    font-family: "Pacifico";
-    font-size: xx-large;
-  }
+.name {
+  font-family: "Pacifico";
+  font-size: xx-large;
+}
 </style>
